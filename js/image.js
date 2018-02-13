f2f7a0b1ddacb3537bca1bbedb36f807858db83c
Andrew Wang <andrewxiaoyu0@gmail.com>
2018-02-13 15:37:09 -0500

    Edited sponsors/edited mobile sponsor formatting

--- js/image.js
+++ js/image.js
@@ -5,10 +5,14 @@ var sponsorImages = [
   ['yext.png', 'yext.com'],
   ['balsamiq.png', 'balsamiq.com'],
   ['mapbox.png', 'mapbox.com'],
+  ['appian.png','appian.com'],
   ['globalrecontech.png', 'globalrecontech.com'],
+  ['octoconsulting.jpg','octoconsulting.com'],
   ['wgssystems.jpg', 'wgssystems.com'],
   ['joinhorizons.svg', 'joinhorizons.com'],
-  ['microsoft.png', 'microsoft.com'] ];
+  ['microsoft.png', 'microsoft.com'],
+  ['clc.png','tjclc.wordpress.com']
+];
 var partnerImages = [
   ['mlh.png', 'mlh.io'],
   ['tjpartnershipfund.jpg','tjpartnershipfund.org']
