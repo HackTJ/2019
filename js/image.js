a367eab23fa57ab3c6677a57fb68fc5038f285e0
Andrew Wang <andrewxiaoyu0@gmail.com>
2018-02-13 23:26:43 -0500

    Added sponsors/updated buttons

--- js/image.js
+++ js/image.js
@@ -1,8 +1,10 @@
 var sponsorImages = [
   ['mantech.jpg', 'mantech.com'],
   ['fanniemae.png', 'fanniemae.com'],
+  ['phone2action.png','phone2action.com'],
   ['caci.png', 'caci.com'],
   ['yext.png', 'yext.com'],
+  ['capitalone.png','capitalone.com'],
   ['balsamiq.png', 'balsamiq.com'],
   ['mapbox.png', 'mapbox.com'],
   ['appian.png','appian.com'],
@@ -10,10 +12,10 @@ var sponsorImages = [
   ['octoconsulting.jpg','octoconsulting.com'],
   ['wgssystems.jpg', 'wgssystems.com'],
   ['joinhorizons.svg', 'joinhorizons.com'],
-  ['microsoft.png', 'microsoft.com'],
-  ['clc.png','tjclc.wordpress.com']
+  ['microsoft.png', 'microsoft.com']
 ];
 var partnerImages = [
   ['mlh.png', 'mlh.io'],
-  ['tjpartnershipfund.jpg','tjpartnershipfund.org']
+  ['tjpartnershipfund.jpg','tjpartnershipfund.org'],
+  ['clc.png','tjclc.wordpress.com']
 ]
